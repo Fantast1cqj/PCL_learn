7cec0554eff999ffee6596bc214795010ada7611
fqj <1422591267@qq.com>
2023-10-11 16:51:10 +0800

    第一次提交

---
+++ 00/build/CMakeFiles/pcl_learn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_learn.dir/pcl_learn.cpp.o"
+  "pcl_learn.pdb"
+  "pcl_learn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 00/pcl_learn.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <pcl/pcl_config.h>
+
+int main()
+{
+    std::cout << "PCL version: " << PCL_VERSION_PRETTY << std::endl;
+    
+    return 0;
+}
