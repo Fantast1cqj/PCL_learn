9ca51c9ffb54a61a3e9324ba3a50eee62e43017e
fqj <1422591267@qq.com>
2023-10-11 20:27:14 +0800

    提交

--- 00/pcl_version.cpp
+++ 00/pcl_version.cpp
@@ -6,5 +6,4 @@ int main()
     std::cout << "PCL version: " << PCL_VERSION_PRETTY << std::endl;
     std::cout << "PCL version111: " << PCL_VERSION_PRETTY << std::endl;
     return 0;
-    // 123123
 }
