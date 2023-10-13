d8dc80a0819533a8910d6a1f69eef18f449beb96
fqj <1422591267@qq.com>
2023-10-13 19:55:03 +0800

    删除历史版本记录,初始化仓库

---
+++ 00/pcl_version.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <pcl/pcl_config.h>
+
+int main()
+{
+    std::cout << "PCL version: " << PCL_VERSION_PRETTY << std::endl;
+    std::cout << "PCL version111: " << PCL_VERSION_PRETTY << std::endl;
+    return 0;
+    // test 6
+}
